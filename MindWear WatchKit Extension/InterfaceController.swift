5d1921a2235ccebce5f07563670030d077b6b5f7
foolaw <f.giessauf@icloud.com>
2020-10-13 10:35:31 +0200

    add CoreLocation

--- MindWear WatchKit Extension/InterfaceController.swift
+++ MindWear WatchKit Extension/InterfaceController.swift
@@ -7,13 +7,40 @@
 
 import WatchKit
 import Foundation
+import CoreLocation
 import Alamofire
 
+class InterfaceController: WKInterfaceController, CLLocationManagerDelegate {
 
-class InterfaceController: WKInterfaceController {
-
+    @IBOutlet var longitudeTextField: WKInterfaceTextField!
+    
+    @IBOutlet var latitudeTextField: WKInterfaceTextField!
+    
+    @IBOutlet var sendLocationButton: WKInterfaceButton!
+    
+    let locationManger = CLLocationManager()
+    
+    var longitude: Double = 0
+    var latitude: Double = 0
+    
+    
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        locationManger.delegate = self
+        self.locationManger.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        self.locationManger.requestWhenInUseAuthorization()
+        self.locationManger.startUpdatingLocation()
+        
+        if locationManger.location?.coordinate != nil {
+            
+            longitude = (locationManger.location?.coordinate.longitude)!
+            latitude = (locationManger.location?.coordinate.latitude)!
+        }
+        
+        print("longitude: " + String(longitude))
+        print("latitude: " + String(latitude))
+        
     }
     
     override func willActivate() {
@@ -24,4 +51,10 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
     }
 
+    @IBAction func buttonPressed() {
+        
+        longitudeTextField.setText(String(longitude))
+        latitudeTextField.setText(String(latitude))
+        
+    }
 }
